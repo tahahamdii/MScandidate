b24cf42077b76590fe378c1911bab5880f47000d
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-11 20:30:38 +0100

    RESTAPI

--- src/main/java/com/microservices/candidate/controllers/CandidatRestAPI.java
+++ src/main/java/com/microservices/candidate/controllers/CandidatRestAPI.java
@@ -1,9 +1,14 @@
 package com.microservices.candidate.controllers;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
+import com.microservices.candidate.CandidatService;
+import com.microservices.candidate.entity.Candidat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CandidatRestAPI {
@@ -12,6 +17,26 @@ public class CandidatRestAPI {
     public String sayHello(){
         System.out.println(title);
         return title;
+    }
+
+    @Autowired
+    private CandidatService candidatService;
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat) {
+        return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.OK);
+    }
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Candidat> updateCandidat(@PathVariable(value = "id") int id,
+                                                         @RequestBody Candidat candidat){
+        return new ResponseEntity<>(candidatService.updateCandidat(id, candidat),
+                HttpStatus.OK);
+    }
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteCandidat(@PathVariable(value ="id") int id){
+        return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/microservices/candidate/repo/CandidatRepository.java
+++ src/main/java/com/microservices/candidate/repo/CandidatRepository.java
@@ -1,7 +1,15 @@
 package com.microservices.candidate.repo;
-
+import org.springframework.data.domain.Pageable;
 import com.microservices.candidate.entity.Candidat;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface CandidatRepository extends JpaRepository<Candidat, Integer> {
-        }
+        @Query("select c from Candidat c where c.nom like :name")
+        public Page<Candidat> candidatByNom(@Param("name") String n, Pageable pageable);
+
+
+}
